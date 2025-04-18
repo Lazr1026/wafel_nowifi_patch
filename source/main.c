aea53d15e4741efe257362a5da1fedbd3763f076
Lazr <68634861+Lazr1026@users.noreply.github.com>
2025-04-18 09:21:27 -0700

    remove unneeded includes

--- source/main.c
+++ source/main.c
@@ -1,13 +1,4 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdarg.h>
-#include <wafel/dynamic.h>
-#include <wafel/ios_dynamic.h>
-#include <wafel/utils.h>
 #include <wafel/patch.h>
-#include <wafel/ios/svc.h>
-#include <wafel/ios/memory.h>
 
 // This fn runs before everything else in kernel mode.
 // It should be used to do extremely early patches
