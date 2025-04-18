9e5f9121acc10cc9e78f4b228460ca316c8c465b
Lazr <68634861+Lazr1026@users.noreply.github.com>
2025-04-18 09:05:18 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,24 @@
+# Stroopwafel plugin to tolerate ISFShax
+
+This plugin for [stroopwafel](https://github.com/shinyquagsire23/stroopwafel) is a IOSU patch to remove the need for the WiFi module.
+
+A stock IOSU would crash after some time without the WiFi module, making playing games impossible. Portablizers will find the most use with this patch.
+
+How it works: https://gist.github.com/GaryOderNichts/357df4dcf4ba69962b10c91c6f1d7240
+
+## How to use
+
+Put the `8wifi.ipx` together with `00core.ipx` from stroopwafel in `sd:/wiiu/ios_plugins` or `slc:/sys/hax/ios_plugins`.
+
+## Building
+
+```bash
+export STROOPWAFEL_ROOT=/path/to/stroopwafel-repo
+make
+```
+
+## Thanks
+
+- [**GaryOderNichts**](https://github.com/GaryOderNichts) for the WiFi patch
+
+- [**shinyquagsire23**](https://github.com/shinyquagsire23) and [**jan-hofmeier**](https://github.com/jan-hofmeier) for stroopwafel
---
+++ source/main.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <wafel/dynamic.h>
+#include <wafel/ios_dynamic.h>
+#include <wafel/utils.h>
+#include <wafel/patch.h>
+#include <wafel/ios/svc.h>
+#include <wafel/ios/memory.h>
+
+// This fn runs before everything else in kernel mode.
+// It should be used to do extremely early patches
+// (ie to BSP and kernel, which launches before MCP)
+// It jumps to the real IOS kernel entry on exit.
+__attribute__((target("arm")))
+void kern_main()
+{
+    // Make sure relocs worked fine and mappings are good
+    debug_printf("we in here WiFi patch kern %p\n", kern_main);
+
+    debug_printf("init_linking symbol at: %08x\n", wafel_find_symbol("init_linking"));
+
+    // Make opening /dev/net/ifmgr/ncl not depend on /dev/net/ifmgr/wd
+    ASM_PATCH_K(0x12309ad4,
+        "mov r1, r5\n"
+        "nop"
+    );
+
+    debug_printf("WiFi patches applied!\n");
+}
+
+// This fn runs before MCP's main thread, and can be used
+// to perform late patches and spawn threads under MCP.
+// It must return.
+void mcp_main()
+{
+
+
+}
